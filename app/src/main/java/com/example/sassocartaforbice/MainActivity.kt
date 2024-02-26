f93222628f752306de30608756a2644203cccaa6
manente.edoardo <edoardo.manente@edu.iisleviponti.it>
2024-02-26 13:27:58 +0100

    Initial commit

--- app/src/main/java/com/example/sassocartaforbice/MainActivity.kt
+++ app/src/main/java/com/example/sassocartaforbice/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
+        //ciao
         text = "Hello $name!",
         modifier = modifier
     )
